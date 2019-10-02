a956693265a26b18941ea1cb5ad5ffa707bf8fa2
Raghav Mecheri <raghav.mecheri@columbia.edu>
2019-10-02 17:31:37 -0400

    Initial working version

---
+++ generate_rubric.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import json
+import sys
+import re
+
+def refactor_rules(gradepoints, namelist):
+	rows = []
+	for rule,points in gradepoints.items():
+		row = {}
+		row["Rule"] = rule
+		row["Deduction"] = points
+		for x in namelist:
+			row[x] = 0
+		rows.append(row)
+	return rows
+
+def are_invalid(name, grades, output):
+	test_name = re.search(".txt$", name)
+	test_grades = re.search(".json$", grades)
+	test_output = re.search(".csv$", output)
+	if((not test_name) or (not test_grades) or (not test_output)):
+		return True
+	return False
+
+if __name__ == "__main__":
+
+	try:
+		NAME_FILE = sys.argv[1]
+		GRADE_FILE = sys.argv[2]
+		OUTPUT_FILE = sys.argv[3]
+	except:
+		print("Please pass three command line arguements!")
+		raise SystemExit
+
+	if(are_invalid(NAME_FILE, GRADE_FILE, OUTPUT_FILE)):
+		print("Please pass three filenames, a .txt namelist, a json rubric, and an output csv file in that order")
+		raise SystemExit
+		
+	print("Extracting names...")
+
+	namelist = open(NAME_FILE,"r").read().split(',')
+
+	while("" in namelist) : 
+	    namelist.remove("")
+
+	print("Extracting grades...")
+
+	with open(GRADE_FILE) as json_file:
+	    gradepoints = json.load(json_file)
+
+	# print(namelist)
+	# print(gradepoints)
+
+	print("Compiling CSV...")
+
+	cols = ["Rule", "Deduction"] + namelist
+	my_csv = pd.DataFrame(columns=cols)
+
+	row_entries = refactor_rules(gradepoints,namelist)
+	my_csv.append(row_entries)
+
+	my_csv.to_csv(OUTPUT_FILE, index=False)
+
+	print("Compiled CSV!")
---
+++ readme.md
@@ -0,0 +1,28 @@
+# RubricGen
+An automated grading rubric generator to make the lives of Instructors easier
+
+# Sample Call
+python3 generate_rubric.py "namelist.txt" "rubric.json" "rubric.csv" <br>
+Sample .txt and .json input files can be found in the samples/ directory. <br>
+It's worth noting that the .json file has a "Total":0 row by design, to hold the total points deducted per student <br>
+
+## Description
+The purpose of this program is to compile a grading rubric spreadsheet automatically, for easy HW grading by TAs and instructors. <br>
+A namelist and a rubric are both required for this program to run, both must be passed as args. A third arguement must also be passed,
+which is the name of the output csv
+
+## Courseworks Namelist Exctraction
+
+To get this list from Courseworks: <br>
+- Go to the People page for the class <br>
+- Set the role to student <br>
+- Run the below JS snippet in your browser console: <br>
+	```javascript
+	targets = document.getElementsByClassName("roster_user_name student_context_card_trigger");
+	namelist = []
+	for(x = 0; x < targets.length; x++) {
+		namelist.push(targets[x].innerText);
+	}
+	console.log(namelist.toString());
+	```
+This should log a comma separated list that this program accepts :)
